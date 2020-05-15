21665f173662ec80deb642740494329cf5c5f294
christerross <47217893+christerross@users.noreply.github.com>
2020-05-15 20:16:49 +0200

    Vue app

---
+++ src/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+      <LoginComponent message="Please Login to view jobs"/>
+</template>
+
+<script>
+import LoginComponent from './components/LoginComponent.vue'
+
+export default {
+  name: 'HomePage',
+  components: {
+    LoginComponent
+  }  
+}
+</script>
+
---
+++ src/JobListingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="[ row ]">
+    <JobListingComponent></JobListingComponent>
+  </div>
+</template>
+
+<script>
+import JobListingComponent from './components/JoblistingComponent.vue'
+export default {
+  name: 'JobListing',
+  components:{
+    JobListingComponent 
+  }
+}
+</script>

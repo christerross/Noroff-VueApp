102a235b652ab2fc347c3ce9447b75581685d6ed
christerross <47217893+christerross@users.noreply.github.com>
2020-05-17 22:57:52 +0200

    Vue App update 2

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  (function () {
+    localStorage.setItem('username', 'Christer');
+    localStorage.setItem('password', '@1234');
+  })();
+
+export default {
+  name: 'app',
+}
+</script>
---
+++ src/components/JobListingComponent.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="[ card ]">
+  <div class="[ row ]">
+    <div class="[ col-sm-4 ]"></div>
+    <div class="[ col-sm-4 ]"><img :src="jobImg" /><br /><br /><br /></div>
+    <div class="[ col-sm-4 ]"></div>
+  </div>
+       <div class="[ card--information ]"><br />
+            <b>Date Created: </b>{{dateCreated}}<br />
+            <b>Title: </b>{{title}}<br />
+            <b>Company Name: </b>{{companyName}}<br />
+            <b>Location: </b>{{location}}<br />
+            <b>Type: </b>{{type}}<br />
+            <b>Description: </b> <span v-html="description"></span><br />
+            <b>How to apply: </b> <span v-html="howToApply"></span><br />
+            <b>Company Website: </b><a :href="website" target="_blank">Click Here</a><br />
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'JobListingComponent',
+  props: ['jobImg','dateCreated','title','companyName','location','type','description','howToApply','website'],
+}
+</script>
+
+<style scoped>
+.card{
+  border: solid 1px black;
+  border-radius: 4px;
+  padding: 0px 20px 30px;
+  margin-top: 30px;
+}
+
+.card img{
+  width: 100%;
+  padding-top: 20px;
+}
+</style>
